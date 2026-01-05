9088de4d922957d0b3d2604e1ee9d9805c4beff1
Fairy Myung <fairymyung@gmil.com>
2026-01-05 16:40:47 +0700

    class - constructor ( from classic to condensed)

--- bin/dart_5.dart
+++ bin/dart_5.dart
@@ -1,7 +1,7 @@
-// * class - constructor
+// * class - constructor ( from classic to condensed)
 
 void main() {
-  Manusia x = Manusia();
+  Manusia x = Manusia(umur: 12, tinggi: 1.6, nama: 'dudu');
   x.beraksi();
   print(x.nama);
   print(x.umur);
@@ -9,18 +9,30 @@ void main() {
 }
 
 class Manusia {
-  Manusia();
+  Manusia({required this.nama, required this.umur, required this.tinggi});
 
-  String nama = 'udin';
-  int umur = 12;
-  double? tinggi;
+  // *constructor condensed with named parameters
+  // Manusia({required this.nama, required this.umur, required this.tinggi});
+
+  // *constructor condensed with positionL parameters
+  // Manusia(this.nama, this.umur, this.tinggi);
+
+  // *constructor classic with named parameters
+  // Manusia({required String namax, required int umurx, required double tinggix}) {
+  //   nama = namax;
+  //   umur = umurx;
+  //   tinggi = tinggix;
+  // *constructor classic with positional parameters
+  // Manusia(String namax, int umurx, double tinggix) {
+  //   nama = namax;
+  //   umur = umurx;
+  //   tinggi = tinggix;
+
+  String nama = 'nanad';
+  int umur = 20;
+  double tinggi = 2.2;
 
   void beraksi() {
-    print('hai');
+    print('hai siapa kamu');
   }
 }
-
-// * sebuah fungsi spesial dari class
-// * mempunyai nama yang sama dengan class nya
-// * dart akan otomatis buat default constructor
-// * default constructor adalah constructor tapi tanpa parameter
