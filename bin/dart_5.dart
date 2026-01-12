4e554035a602f37122d541781222e70c605b194e
Fairy Myung <fairymyung@gmil.com>
2026-01-12 22:11:05 +0700

    named constructor

--- bin/dart_5.dart
+++ bin/dart_5.dart
@@ -1,15 +1,18 @@
-// * default constructor
-
+// * named constructor
 void main() {
-  // var s = Siswa(2, 1);
-  var s = Siswa();
-  print(s.kelas);
-  print(s.rangking);
+  //var x = Manusia.pebasket('tari');
+  // var x = Manusia();
+  var x = Manusia.budi(178, 22);
+  print(x.nama);
+  print(x.tinggi);
+  print(x.umur);
 }
 
-class Siswa {
-  // Siswa();
-  // Siswa(this.kelas, this.rangking);
-  int kelas = 10;
-  int rangking = 1;
+class Manusia {
+  Manusia(this.nama, this.tinggi, this.umur);
+  Manusia.pebasket(this.nama) : tinggi = 190, umur = 19;
+  Manusia.budi(this.tinggi, this.umur) : nama = 'budi';
+  String nama;
+  int umur;
+  int tinggi;
 }
